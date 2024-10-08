0bea3d2cee3a17315f98b05fcf4dd73c7c77de59
Luis Antunes <luisantunes0914@gmail.com>
2024-10-08 17:56:02 -0300

    add react/js/ts suport coding to nvim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -51,88 +51,119 @@ vim.cmd([[let &t_Cs = "\e[4:3m"]])
 vim.cmd([[au BufNewFile,BufRead *.astro setf astro]])
 vim.cmd([[au BufNewFile,BufRead Podfile setf ruby]])
 
+vim.filetype.add({
+  extension = {
+    js = "javascript",
+    jsx = "javascriptreact",
+    ts = "typescript",
+    tsx = "typescriptreact",
+  },
+  filename = {
+    [".eslintrc"] = "json",
+    [".prettierrc"] = "json",
+  },
+  pattern = {
+    [".*%.js"] = "javascript",
+    [".*%.jsx"] = "javascriptreact",
+    [".*%.ts"] = "typescript",
+    [".*%.tsx"] = "typescriptreact",
+  },
+})
+
 -- HTML: 2 espaços para indentação
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "html", "css", "ejs", "javascript", "typescript", "vue", "jsx", "tsx" },
-    callback = function()
-        vim.opt_local.tabstop = 2
-        vim.opt_local.shiftwidth = 2
-        vim.opt_local.expandtab = true
-    end,
+  pattern = {
+    "html",
+    "css",
+    "ejs",
+    "vue",
+    "jsx",
+    "tsx",
+    "javascript",
+    "typescript",
+    "javascriptreact",
+    "typescriptreact",
+  },
+  callback = function()
+    vim.opt_local.expandtab = true
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+  end,
 })
 
 -- Configurações do Obsidian
 local obsidian_mappings = {
-    { "n", "<leader>oc", "<cmd>lua require('obsidian').util.toggle_checkbox()<CR>", "Obsidian Check Checkbox" },
-    { "n", "<leader>ot", "<cmd>ObsidianTemplate<CR>",                               "Insert Obsidian Template" },
-    { "n", "<leader>oo", "<cmd>ObsidianOpen<CR>",                                   "Open in Obsidian App" },
-    { "n", "<leader>ob", "<cmd>ObsidianBacklinks<CR>",                              "Show ObsidianBacklinks" },
-    { "n", "<leader>ol", "<cmd>ObsidianLinks<CR>",                                  "Show ObsidianLinks" },
-    { "n", "<leader>on", "<cmd>ObsidianNew<CR>",                                    "Create New Note" },
-    { "n", "<leader>os", "<cmd>ObsidianSearch<CR>",                                 "Search Obsidian" },
-    { "n", "<leader>oq", "<cmd>ObsidianQuickSwitch<CR>",                            "Quick Switch" },
+  { "n", "<leader>oc", "<cmd>lua require('obsidian').util.toggle_checkbox()<CR>", "Obsidian Check Checkbox" },
+  { "n", "<leader>ot", "<cmd>ObsidianTemplate<CR>", "Insert Obsidian Template" },
+  { "n", "<leader>oo", "<cmd>ObsidianOpen<CR>", "Open in Obsidian App" },
+  { "n", "<leader>ob", "<cmd>ObsidianBacklinks<CR>", "Show ObsidianBacklinks" },
+  { "n", "<leader>ol", "<cmd>ObsidianLinks<CR>", "Show ObsidianLinks" },
+  { "n", "<leader>on", "<cmd>ObsidianNew<CR>", "Create New Note" },
+  { "n", "<leader>os", "<cmd>ObsidianSearch<CR>", "Search Obsidian" },
+  { "n", "<leader>oq", "<cmd>ObsidianQuickSwitch<CR>", "Quick Switch" },
 }
 
 for _, mapping in ipairs(obsidian_mappings) do
-    vim.keymap.set(mapping[1], mapping[2], mapping[3], { desc = mapping[4] })
+  vim.keymap.set(mapping[1], mapping[2], mapping[3], { desc = mapping[4] })
 end
 
 -- Configurações específicas para versões mais recentes do Neovim
 if vim.fn.has("nvim-0.8") == 1 then
-    vim.opt.cmdheight = 0
+  vim.opt.cmdheight = 0
 end
 
 -- Configuração específica para arquivos Markdown
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "markdown",
-    callback = function()
-        -- Quebra de linha
-        vim.opt_local.wrap = true
-        vim.opt_local.linebreak = true -- Quebra linhas em palavras completas
-        vim.opt_local.textwidth = 80
-        vim.opt_local.formatoptions = "tcqjn"
-        -- t: quebra automática de texto
-        -- c: quebra automática de comentários
-        -- q: permite formatar comentários com "gq"
-        -- j: remove comentário líder ao juntar linhas
-        -- n: reconhece listas numeradas
+  pattern = "markdown",
+  callback = function()
+    -- Quebra de linha
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true -- Quebra linhas em palavras completas
+    vim.opt_local.textwidth = 80
+    vim.opt_local.formatoptions = "tcqjn"
+    -- t: quebra automática de texto
+    -- c: quebra automática de comentários
+    -- q: permite formatar comentários com "gq"
+    -- j: remove comentário líder ao juntar linhas
+    -- n: reconhece listas numeradas
 
-        -- Correção ortográfica
-        vim.opt_local.spell = true
-        vim.opt_local.spelllang = "pt_br,en_us"
+    -- Correção ortográfica
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = "pt_br,en_us"
 
-        -- Destaque de sintaxe aprimorado
-        vim.opt_local.conceallevel = 2
+    -- Destaque de sintaxe aprimorado
+    vim.opt_local.conceallevel = 2
 
-        -- Indentação
-        vim.opt_local.tabstop = 2
-        vim.opt_local.shiftwidth = 2
-        vim.opt_local.softtabstop = 2
-        vim.opt_local.expandtab = true
+    -- Indentação
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.expandtab = true
 
-        -- Folding (dobradura de código)
-        vim.opt_local.foldmethod = "expr"
-        vim.opt_local.foldexpr = "nvim_treesitter#foldexpr()"
-        vim.opt_local.foldenable = false -- Começa com todas as dobras abertas
+    -- Folding (dobradura de código)
+    vim.opt_local.foldmethod = "expr"
+    vim.opt_local.foldexpr = "nvim_treesitter#foldexpr()"
+    vim.opt_local.foldenable = false -- Começa com todas as dobras abertas
 
-        -- Navegação melhorada
-        vim.opt_local.suffixesadd:append(".md")
-        vim.opt_local.path:append("**")
+    -- Navegação melhorada
+    vim.opt_local.suffixesadd:append(".md")
+    vim.opt_local.path:append("**")
 
-        -- Completação automática de listas
-        vim.opt_local.comments = "b:*,b:-,b:+,n:>"
+    -- Completação automática de listas
+    vim.opt_local.comments = "b:*,b:-,b:+,n:>"
 
-        -- Mapeamentos específicos para Markdown
-        vim.keymap.set("n", "<leader>mp", ":MarkdownPreview<CR>", { buffer = true, desc = "Markdown Preview" })
-        vim.keymap.set("n", "<leader>mt", ":TableModeToggle<CR>", { buffer = true, desc = "Toggle Table Mode" })
+    -- Mapeamentos específicos para Markdown
+    vim.keymap.set("n", "<leader>mp", ":MarkdownPreview<CR>", { buffer = true, desc = "Markdown Preview" })
+    vim.keymap.set("n", "<leader>mt", ":TableModeToggle<CR>", { buffer = true, desc = "Toggle Table Mode" })
 
-        -- Comando para reformatar parágrafos
-        vim.api.nvim_buf_create_user_command(0, "WrapParagraph", function()
-            vim.cmd("normal! gqip")
-        end, { desc = "Wrap current paragraph" })
+    -- Comando para reformatar parágrafos
+    vim.api.nvim_buf_create_user_command(0, "WrapParagraph", function()
+      vim.cmd("normal! gqip")
+    end, { desc = "Wrap current paragraph" })
 
-        vim.keymap.set("n", "<leader>mw", ":WrapParagraph<CR>", { buffer = true, desc = "Wrap Paragraph" })
-    end,
+    vim.keymap.set("n", "<leader>mw", ":WrapParagraph<CR>", { buffer = true, desc = "Wrap Paragraph" })
+  end,
 })
 
 -- Configurações do Prettier
---
+++ .config/nvim/lua/plugins/extras/lang/javascript.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        -- Configuração do tsserver (principal LSP para TS/JS)
+        tsserver = {
+          settings = {
+            typescript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "all",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+              },
+            },
+            javascript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "all",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+              },
+            },
+          },
+        },
+        -- Configuração do ESLint
+        eslint = {
+          settings = {
+            -- Suas configurações específicas do ESLint aqui
+          },
+        },
+      },
+    },
+  },
+}
